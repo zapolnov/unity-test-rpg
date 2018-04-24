934a21f7a7ac58f448be13e60d23ace63c4364cd
Nikolay Zapolnov <zapolnov@gmail.com>
2018-04-24 08:57:54 +0200

    Player can walk around.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿
+using UnityEngine;
+
+namespace Game
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float strafeSpeed;
+        public float forwardSpeed;
+        private bool attacking;
+
+        private Animator mAnimator;
+        private Rigidbody mRigidBody;
+
+        void Start()
+        {
+            mAnimator = GetComponent<Animator>();
+            mRigidBody = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate()
+        {
+            if (Input.GetButton("Fire1")) {
+                attacking = true;
+            } else {
+                if (attacking) {
+                    var currentAnim = mAnimator.GetCurrentAnimatorStateInfo(0);
+                    if (currentAnim.IsName("Attack") && currentAnim.normalizedTime >= 1.0f)
+                        attacking = false;
+                }
+            }
+
+            if (attacking) {
+                mRigidBody.velocity = new Vector3();
+            } else {
+                float x = Input.GetAxis("Horizontal") * strafeSpeed;
+                float y = Input.GetAxis("Vertical") * forwardSpeed;
+                Vector3 direction = transform.forward * y + transform.right * x;
+                mRigidBody.velocity = direction;
+            }
+
+            mAnimator.SetFloat("speed", mRigidBody.velocity.magnitude);
+            mAnimator.SetBool("attack", attacking);
+        }
+    }
+}
