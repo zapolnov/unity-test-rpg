14469d20689ff78fb87c1b1344571a742c1cb697
Nikolay Zapolnov <zapolnov@gmail.com>
2018-04-24 09:16:14 +0200

    Started to implement RPG system.

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+namespace Game
+{
+    public class Character : MonoBehaviour
+    {
+        public CharacterDefinition definition;
+
+        private void Awake()
+        {
+        }
+    }
+}
---
+++ Assets/Scripts/CharacterDefinition.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+namespace Game
+{
+    [CreateAssetMenu]
+    public class CharacterDefinition : ScriptableObject
+    {
+        public int level;
+        public float health;
+        public float armor;
+        public float damage;
+        public float givesExp;
+    }
+}
