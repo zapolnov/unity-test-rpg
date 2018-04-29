fcf5d1f0734efb5581e31d43ce849af1c0f719ff
Nikolay Zapolnov <zapolnov@gmail.com>
2018-04-29 19:35:36 +0200

    Improve user experience + bake lighting.

--- Assets/Scripts/Classes/AbstractInteractable.cs
+++ Assets/Scripts/Classes/AbstractInteractable.cs
@@ -1,37 +1,57 @@
 ﻿
 using UnityEngine;
+using System.Collections.Generic;
 
 namespace Game
 {
     public abstract class AbstractInteractable : MonoBehaviour
     {
-        public GameObject highlightTarget;
+        private struct MaterialPatcher
+        {
+            private Renderer mRenderer;
+            private Material[] mOriginalMaterials;
+            private Material[] mPatchedMaterials;
+
+            public MaterialPatcher(GameObject highlightTarget, Color highlightColor)
+            {
+                    mRenderer = highlightTarget.GetComponent<Renderer>();
+                    mOriginalMaterials = mRenderer.sharedMaterials;
+                    mPatchedMaterials = new Material[mOriginalMaterials.Length];
+                    for (int i = 0; i < mOriginalMaterials.Length; i++) {
+                        mPatchedMaterials[i] = new Material(mOriginalMaterials[i]);
+                        mPatchedMaterials[i].color = highlightColor;
+                    }
+            }
+
+            public void SetHighlighted(bool flag)
+            {
+                if (flag)
+                    mRenderer.sharedMaterials = mPatchedMaterials;
+                else
+                    mRenderer.sharedMaterials = mOriginalMaterials;
+            }
+        }
+
+        public GameObject[] highlightTargets;
         public Color highlightColor = Color.green;
 
-        private Renderer mRenderer;
-        private Material[] mOriginalMaterials;
-        private Material[] mPatchedMaterials;
+        private readonly List<MaterialPatcher> mMaterialPatchers = new List<MaterialPatcher>();
 
         void Awake()
         {
-            if (highlightTarget == null)
-                highlightTarget = gameObject;
-
-            mRenderer = highlightTarget.GetComponent<Renderer>();
-            mOriginalMaterials = mRenderer.sharedMaterials;
-            mPatchedMaterials = new Material[mOriginalMaterials.Length];
-            for (int i = 0; i < mOriginalMaterials.Length; i++) {
-                mPatchedMaterials[i] = new Material(mOriginalMaterials[i]);
-                mPatchedMaterials[i].color = highlightColor;
+            if (highlightTargets == null)
+                mMaterialPatchers.Add(new MaterialPatcher(gameObject, highlightColor));
+            else {
+                foreach (var highlightTarget in highlightTargets)
+                    mMaterialPatchers.Add(new MaterialPatcher(highlightTarget, highlightColor));
             }
         }
 
         void LateUpdate()
         {
-            if (GameController.Instance.playerController.targetObjectForInteraction == this)
-                mRenderer.sharedMaterials = mPatchedMaterials;
-            else
-                mRenderer.sharedMaterials = mOriginalMaterials;
+            bool highlighted = (GameController.Instance.playerController.targetObjectForInteraction == this);
+            foreach (var patcher in mMaterialPatchers)
+                patcher.SetHighlighted(highlighted);
         }
 
         public virtual bool CanInteract() { return true; }
