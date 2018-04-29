74f655e0e1e3cb3b075beb45a981c3ad3c487c3d
Nikolay Zapolnov <zapolnov@gmail.com>
2018-04-30 00:39:33 +0200

    Rotate enemies' health bar towards the camera.

---
+++ Assets/Scripts/Components/RotateTowardsCamera.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+namespace Game
+{
+    public class RotateTowardsCamera : MonoBehaviour
+    {
+        void Update()
+        {
+            var camera = Camera.main;
+            if (camera == null)
+                return;
+
+            var camTrans = camera.transform;
+            transform.LookAt(transform.position + camTrans.rotation * Vector3.forward, camTrans.rotation * Vector3.up);
+        }
+    }
+}
